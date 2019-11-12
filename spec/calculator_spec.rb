84c97bf5f77c050f5b3555792a21bf00dc83f009
algebraic-designer-6218 <jmiller@archersystems.us>
2019-11-12 19:29:07 +0000

    Done.

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,6 +1,6 @@
 # Code your solution in calculator.rb
 
-describe "./calculator.rb" do
+decribe calculator.rb do
 
   it "contains a local variable called first_number that is assigned to a number" do
     first_number = get_variable_from_file('./calculator.rb', "first_number")
